e601b3ab98e3d129e2fd55da1b24b088512cbea1
kim <rla5378@naver.com>
2021-08-12 10:26:28 +0900

    프로젝트 생성

--- src/com/care/def/MainClass.java
+++ src/com/care/def/MainClass.java
@@ -1,5 +1,28 @@
 package com.care.def;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("---∞°¿¸¡¶«∞---");
+		while (true) {
+			System.out.println("1.≥√¿Â∞Ì");
+			System.out.println("2.TV");
+			System.out.println("3.ºº≈π±‚");
+			System.out.println(">>>: ");
+			num = input.nextInt();
+			switch (num) {
+			case 1:
+				System.out.println("≥√¿Â∞Ì");
+				break;
+			case 2:
+				System.out.println("Tv");
+				break;
+			case 3:
+				break;
+			}
+		}
+	}
 }
