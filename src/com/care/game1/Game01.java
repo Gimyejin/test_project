bdc481d23881a38647ddc04edf439ab3031e2552
leenayoung <lny950@naver.com>
2021-08-12 11:41:49 +0900

    game01기능완성

--- src/com/care/game1/Game01.java
+++ src/com/care/game1/Game01.java
@@ -1,5 +1,25 @@
 package com.care.game1;
 
 public class Game01 {
-
+	public void display() {
+		System.out.println("======= 게임1 ======");
+		
+	}
+	public class TV_choheewon {
+        public void display() {
+                System.out.println("냉장고 하는 기능입니다");
+                Scanner in = new Scanner(System.in);
+                int num;
+                System.out.println("1.올림, 2.내림");
+                num=in.nextInt();
+                if(num==1) tempUp();
+                else tempDown();
+        }
+        private void tempUp() {
+                System.out.println("온도를 올립니다");
+        }
+        private void tempDown() {
+                System.out.println("온도를 낮춤니다");
+        }
+}
 }
